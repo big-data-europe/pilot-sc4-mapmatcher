e1057f067a3eeb26bbc6a592b7765b75b7aecf3c
Luigi Selmi <selmi_luigi@hotmail.com>
2016-04-30 18:11:05 +0200

    added logger

--- src/main/java/eu/bde/sc4pilot/mapmatch/MapMatch.java
+++ src/main/java/eu/bde/sc4pilot/mapmatch/MapMatch.java
@@ -1,16 +1,47 @@
 package eu.bde.sc4pilot.mapmatch;
 
 import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPMismatchException;
 import org.rosuda.REngine.Rserve.RConnection;
 import org.rosuda.REngine.Rserve.RserveException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MapMatch {
+	
+	private static final Logger log = LoggerFactory.getLogger(MapMatch.class);
 
-	public static void main(String[] args) throws Exception {
-		RConnection c = new RConnection();
-		REXP x = c.eval("R.version.string");
-		System.out.println(x.asString());
-
+	public static void main(String[] args) { 
+	    RConnection c = initRserve();
+		try {
+			
+			REXP x = c.eval("R.version.string");
+			
+			log.info(x.asString());
+			
+			
+		} catch (RserveException e) {		
+			e.printStackTrace();
+		} catch (REXPMismatchException e) {			
+			e.printStackTrace();
+		} finally {
+			c.close();
+		}
+	}
+	
+	private static RConnection initRserve() {
+		RConnection c = null;
+		try {
+			c = new RConnection();
+			REXP x = c.eval("R.version.string");
+			log.info(x.asString());;
+			
+		} catch (RserveException e) {			
+			e.printStackTrace();
+		} catch (REXPMismatchException e) {			
+			e.printStackTrace();
+		}
+		return c;
 	}
 
 }
